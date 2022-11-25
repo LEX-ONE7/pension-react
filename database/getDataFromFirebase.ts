03650d09df2a67f4f6e4e632680cc27a9e12d2b7
LEX-ONE7 <118214894+LEX-ONE7@users.noreply.github.com>
2022-11-25 09:51:39 -1000

    2 files modified

--- database/getDataFromFirebase.ts
+++ database/getDataFromFirebase.ts
@@ -20,7 +20,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+const db = getFirestore(app );
 
 export const getDataListFromFirebase = async (collectionName: string) => {
   const querySnapshot = await getDocs(collection(db, collectionName));
