018a57cee714686dda8240ea7b240165aef5e3f5
LEX-ONE7 <118214894+LEX-ONE7@users.noreply.github.com>
2022-11-25 13:27:09 -1000

    2 files modified, 2 added

--- components/Chambre.tsx
+++ components/Chambre.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
 import Navbar from '../components/Navbar';
 import { getDataFromFirebase } from '../database/getDataFromFirebase';
-import SliderBungalows from './SliderBungalows';
+import SliderChambre from './SliderChambre';
 
-const LayoutBungalows = () => (
+const Chambre = () => (
   <div className="container-fluid text-center ">
     <div className="row">
       <div className="col-12 col-md-8 p-0">
-        <SliderBungalows />
+        <SliderChambre />
       </div>
       <div className="col-12 col-md-4 d-flex align-items-center column justify-content-center bg-info">
         <h3 className="col">Our Bungalows</h3>
@@ -16,4 +16,4 @@ const LayoutBungalows = () => (
     </div>
   </div>
 );
-export default LayoutBungalows;
+export default Chambre;
--- pages/Bungalows.tsx
+++ pages/Bungalows.tsx
@@ -7,6 +7,7 @@ const Bungalows = () => {
     <div>
       <Navbar />
       <LayoutBungalows />
+      <Chambre />
     </div>
   );
 };
