b4024654ad3b24ce841c40c88a9ad0e37049de96
LEX-ONE7 <118214894+LEX-ONE7@users.noreply.github.com>
2022-11-25 12:19:31 -1000

    1 file modified

--- components/LayoutBungalows.tsx
+++ components/LayoutBungalows.tsx
@@ -1,28 +1,18 @@
 import * as React from 'react';
 import Navbar from '../components/Navbar';
 import { getDataFromFirebase } from '../database/getDataFromFirebase';
-
-const [data, setData] = React.useState([]);
-React.useEffect(() => {
-  (async () => {
-    const response = await getDataFromFirebase('layout-bungalows');
-
-    console.log(response);
-    setData(response);
-  })();
-}, []);
+import Slider from './Slider';
 
 const LayoutBungalows = () => (
-  <div className="container text-center">
+  <div className="container-fluid text-center ">
     <div className="row">
-      <div className="col">col</div>
-      <div className="col">col</div>
-      <div className="col">col</div>
-      <div className="col">col</div>
-    </div>
-    <div className="row">
-      <div className="col-8">col-8</div>
-      <div className="col-4">col-4</div>
+      <div className="col-8 p-0">
+        <Slider />
+      </div>
+      <div className="col-4 d-flex align-items-center column justify-content-center bg-info">
+        <h3>Découvrez notre nouveau bungalow</h3>
+        <p>Je suis un texte sans importance</p>
+      </div>
     </div>
   </div>
 );
