bc72e13645ccd31ddb0ad18254a3318429124e52
LEX-ONE7 <118214894+LEX-ONE7@users.noreply.github.com>
2022-11-25 12:36:58 -1000

    1 file modified

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -22,7 +22,7 @@ const Navbar = () => (
           <a className="nav-link active" aria-current="page" href="#">
             Accueil
           </a>
-          <a className="nav-link" href="../components/LayoutBungalows">
+          <a className="nav-link" href="../pages/Bungalows">
             Bungalows
           </a>
           <a className="nav-link" href="#">
